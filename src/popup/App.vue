d4bf457cfc1b15590f0c27ae783ec84b424892b9
pengfei.Ge <pengfei.ge@chintanneng.com>
2025-09-25 14:24:39 +0800

    feat: use js

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,11 +1,11 @@
 <template>
   <el-card>
     <h2>Hello from Vue 3 + Element Plus!</h2>
-    <el-button type="primary" @click="onClick">点击我</el-button>
+    <el-button type="primary" @click="onClick">点击我1js</el-button>
   </el-card>
 </template>
 
-<script setup lang="ts">
+<script setup>
 const onClick = () => {
   ElMessage.success('你点击了按钮！')
 }
